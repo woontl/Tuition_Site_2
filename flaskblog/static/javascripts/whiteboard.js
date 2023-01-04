85abd81480fcadd7c44d33b340d32ffdb27ac539
Woon Tian Li <woontl96@gmail.com>
2023-01-04 15:40:06 +0800

    test

--- flaskblog/static/javascripts/whiteboard.js
+++ flaskblog/static/javascripts/whiteboard.js
@@ -1,269 +1,207 @@
+
 window.addEventListener('load', ()=> {
+    var canvas = document.querySelector("#canvas");
+    var ctx = canvas.getContext('2d')
+    const canvas_parent = document.querySelector("#canvas_parent")
+
+    //reload canvas with dataURL
+    var url = document.getElementById('workings').value;
+    var img = new Image()
+    img.src = url
+    img.onload = () => { ctx.drawImage(img, 0, 0); };
+    img.src = url
+
+
+    //Resizing
     function onResize() {
         canvas.height = window.innerHeight;
         canvas.width = canvas_parent.offsetWidth;
     };
+
+
+    //variables
+    let painting = false;
+    var eraser_state = false;
+    let temp_color = ''
+    let temp_font = ''
+    var submitBtn = $('#ans');
+    var clearBtn = $('#canvas-clear-btn');
+    var saveBtn = $('#canvas-save-btn');
+    var refreshBtn = $('#canvas-refresh-btn');
+    var eraserBtn = $('#canvas-eraser-btn');
+    var colorBtn = $('.color-btn');
+    colorBtn.css('border', '1px solid ' + 'black');  
+    colorBtn.css('background-color', 'black');
+    var blackcolorBtn = $('#canvas-black-color-btn');
+    var redcolorBtn = $('#canvas-red-color-btn');
+    var bluecolorBtn = $('#canvas-blue-color-btn');
+    var greencolorBtn = $('#canvas-green-color-btn');
+    var yellowcolorBtn = $('#canvas-yellow-color-btn');
+    var currentcolor = 'black';
+    var currentfont = 1;
+    var fontBtn = $('.font-btn');
+    var smallfontBtn = $('#canvas-small-font-btn');
+    var mediumfontBtn = $('#canvas-medium-font-btn');
+    var largefontBtn = $('#canvas-large-font-btn');
+    var pointerCount = 0
+
+    //functions
+    function startPosition(e){
+        pointerCount+=1
+        if (pointerCount==1){
+            painting = true;
+            draw(e, currentcolor)
+        }
+    }
+
+    function finishedPosition(){
+        pointerCount=0
+        painting = false;
+        ctx.beginPath();
+    }
+
+    function eraseBoard() {
+        if (eraser_state == false) {
+            temp_color = currentcolor
+            temp_font = currentfont
+            currentcolor = 'white';
+            currentfont = 5;
+            eraserBtn.css('border', '1px solid ' + 'blue');  
+            eraserBtn.css('background-color', 'blue');
+            eraser_state = true;
+        } else if (eraser_state == true) {
+            currentcolor = temp_color;
+            currentfont = temp_font;
+            eraserBtn.css('border', '1px solid ' + 'white');  
+            eraserBtn.css('background-color', 'white');
+            eraser_state = false;
+        } 
+    };
+    function changeblackColor() {
+        currentcolor = 'black';
+        colorBtn.css('border', '1px solid ' + currentcolor);  
+        colorBtn.css('background-color', currentcolor);
+    };
+    function changeredColor() {
+        currentcolor = 'red';
+        colorBtn.css('border', '1px solid ' + currentcolor);  
+        colorBtn.css('background-color', currentcolor);
+    };
+    function changeblueColor() {
+        currentcolor = 'blue';
+        colorBtn.css('border', '1px solid ' + currentcolor);  
+        colorBtn.css('background-color', currentcolor);
+    };
+    function changegreenColor() {
+        currentcolor = 'green';
+        colorBtn.css('border', '1px solid ' + currentcolor);  
+        colorBtn.css('background-color', currentcolor);
+    };
+    function changeyellowColor() {
+        currentcolor = 'yellow';
+        colorBtn.css('border', '1px solid ' + currentcolor);  
+        colorBtn.css('background-color', currentcolor);
+    };
+    function changesmallfont() {
+        currentfont = 1
+    };
+    function changemediumfont() {
+        currentfont = 5
+    };
+    function changelargefont() {
+        currentfont = 10
+    };
+
+    function draw(e, currentcolor){
+        if (!painting) return;
+        ctx.strokeStyle = currentcolor;
+        ctx.lineWidth = currentfont;
+        ctx.lineCap = "round";
+        
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        ctx.beginpath();
+        ctx.moveTo(e.offsetX, e.offsetY);
+    }
+
+    function clearBoard() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    };
+
+    function saveURL() {
+        var canvas_out = document.querySelector("#canvas");
+        var dataURL = canvas_out.toDataURL('image/png');
+        document.getElementById('workings').value = dataURL;
+    }
+    
+    // EventListeners
+    // canvas.addEventListener('mousedown', startPosition);
+    // canvas.addEventListener('mouseup', finishedPosition);
+    // canvas.addEventListener('mouseout', finishedPosition);
+    // canvas.addEventListener('mousemove', draw);
+    // canvas.addEventListener('touchstart', function(e){
+    //     console.log(123)
+    //     startPosition()
+    // });
+    // canvas.addEventListener('touchend', function(e){
+    //     finishedPosition()
+    // });
+    // canvas.addEventListener('touchmove', function(e){
+    //     draw()
+    // });
+    canvas.addEventListener('pointerdown', startPosition);
+    canvas.addEventListener('pointerup', finishedPosition);
+    canvas.addEventListener('pointerout', finishedPosition);
+    canvas.addEventListener('pointermove', draw);
+    // canvas.addEventListener('pointerdown', function(e){
+    //     if (e.touches.length==1){
+    //         startPosition
+    //     }
+    // });
+    // canvas.addEventListener('pointerup', function(e){
+    //     if (e.touches.length==1){
+    //         finishedPosition
+    //     }
+    // });
+    // canvas.addEventListener('pointerout', function(e){
+    //     if (e.touches.length==1){
+    //         finishedPosition
+    //     }
+    // });
+    // canvas.addEventListener('pointermove', function(e){
+    //     if (e.touches.length==1){
+    //         draw
+    //     }
+    // });
+
+    // canvas.addEventListener('touchstart', function (e){
+    //     if (e.touches.length==1){
+    //         startPosition
+    //     }
+    // });
+    // canvas.addEventListener('touchmove', function (e){
+    //     if (e.touches.length==1){
+    //         draw
+    //     }
+    // });
+    // canvas.addEventListener('touchend', finishedPosition);
+    // canvas.addEventListener('touchcancel', finishedPosition);
+
+    //onclick buttons
+    clearBtn.on('click', clearBoard);
+    refreshBtn.on('click', saveURL);
+    saveBtn.on('click', saveURL);
+    eraserBtn.on('click', eraseBoard);
+    blackcolorBtn.on('click', changeblackColor);
+    redcolorBtn.on('click', changeredColor);
+    bluecolorBtn.on('click', changeblueColor);
+    greencolorBtn.on('click', changegreenColor);
+    yellowcolorBtn.on('click', changeyellowColor);
+    smallfontBtn.on('click', changesmallfont);
+    mediumfontBtn.on('click', changemediumfont);
+    largefontBtn.on('click', changelargefont);
+    submitBtn.on('click', saveURL);
+
     window.addEventListener('resize', onResize);
     onResize();
-})
-
-// get our canvas element
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-
-// disable right clicking
-document.oncontextmenu = function () {
-    return false;
-}
-
-// list of all strokes drawn
-const drawings = [];
-
-// coordinates of our cursor
-let cursorX;
-let cursorY;
-let prevCursorX;
-let prevCursorY;
-
-// distance from origin
-let offsetX = 0;
-let offsetY = 0;
-
-// zoom amount
-let scale = 1;
-
-// convert coordinates
-function toScreenX(xTrue) {
-    return (xTrue + offsetX) * scale;
-}
-function toScreenY(yTrue) {
-    return (yTrue + offsetY) * scale;
-}
-function toTrueX(xScreen) {
-    return (xScreen / scale) - offsetX;
-}
-function toTrueY(yScreen) {
-    return (yScreen / scale) - offsetY;
-}
-function trueHeight() {
-    return canvas.clientHeight / scale;
-}
-function trueWidth() {
-    return canvas.clientWidth / scale;
-}
-
-function redrawCanvas() {
-    // set the canvas to the size of the window
-    canvas.width = document.body.clientWidth;
-    canvas.height = document.body.clientHeight;
-
-    context.fillStyle = '#fff';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < drawings.length; i++) {
-        const line = drawings[i];
-        drawLine(toScreenX(line.x0), toScreenY(line.y0), toScreenX(line.x1), toScreenY(line.y1));
-    }
-}
-redrawCanvas();
-
-// if the window changes size, redraw the canvas
-window.addEventListener("resize", (event) => {
-    redrawCanvas();
 });
-
-// Mouse Event Handlers
-canvas.addEventListener('pointerdown', onMouseDown);
-canvas.addEventListener('pointerup', onMouseUp, false);
-canvas.addEventListener('pointerout', onMouseUp, false);
-canvas.addEventListener('pointermove', onMouseMove, false);
-canvas.addEventListener('wheel', onMouseWheel, false);
-
-
-// Touch Event Handlers 
-canvas.addEventListener('touchstart', onTouchStart);
-canvas.addEventListener('touchend', onTouchEnd);
-canvas.addEventListener('touchcancel', onTouchEnd);
-canvas.addEventListener('touchmove', onTouchMove);
-
-
-// mouse functions
-let leftMouseDown = false;
-let rightMouseDown = false;
-function onMouseDown(event) {
-
-    // detect left clicks
-    if (event.button == 0) {
-        leftMouseDown = true;
-        rightMouseDown = false;
-    }
-    // detect right clicks
-    if (event.button == 2) {
-        rightMouseDown = true;
-        leftMouseDown = false;
-    }
-
-    // update the cursor coordinates
-    cursorX = event.offsetX;
-    cursorY = event.offsetY;
-    prevCursorX = event.offsetX;
-    prevCursorY = event.offsetY;
-}
-function onMouseMove(event) {
-    // get mouse position
-    cursorX = event.offsetX;
-    cursorY = event.offsetY;
-    const scaledX = toTrueX(cursorX);
-    const scaledY = toTrueY(cursorY);
-    const prevScaledX = toTrueX(prevCursorX);
-    const prevScaledY = toTrueY(prevCursorY);
-
-    if (leftMouseDown) {
-        // add the line to our drawing history
-        drawings.push({
-            x0: prevScaledX,
-            y0: prevScaledY,
-            x1: scaledX,
-            y1: scaledY
-        })
-        // draw a line
-        drawLine(prevCursorX, prevCursorY, cursorX, cursorY);
-    }
-    if (rightMouseDown) {
-        // move the screen
-        offsetX += (cursorX - prevCursorX) / scale;
-        offsetY += (cursorY - prevCursorY) / scale;
-        redrawCanvas();
-    }
-    prevCursorX = cursorX;
-    prevCursorY = cursorY;
-}
-function onMouseUp() {
-    leftMouseDown = false;
-    rightMouseDown = false;
-}
-function onMouseWheel(event) {
-    const deltaY = event.deltaY;
-    const scaleAmount = -deltaY / 500;
-    scale = scale * (1 + scaleAmount);
-
-    // zoom the page based on where the cursor is
-    var distX = event.offsetX / canvas.clientWidth;
-    var distY = event.offsetY / canvas.clientHeight;
-
-    // calculate how much we need to zoom
-    const unitsZoomedX = trueWidth() * scaleAmount;
-    const unitsZoomedY = trueHeight() * scaleAmount;
-
-    const unitsAddLeft = unitsZoomedX * distX;
-    const unitsAddTop = unitsZoomedY * distY;
-
-    offsetX -= unitsAddLeft;
-    offsetY -= unitsAddTop;
-
-    redrawCanvas();
-}
-function drawLine(x0, y0, x1, y1) {
-    context.beginPath();
-    context.moveTo(x0, y0);
-    context.lineTo(x1, y1);
-    context.strokeStyle = '#000';
-    context.lineWidth = 2;
-    context.stroke();
-}
-
-// touch functions
-const prevTouches = [null, null]; // up to 2 touches
-let singleTouch = false;
-let doubleTouch = false;
-function onTouchStart(event) {
-    if (event.touches.length == 1) {
-        singleTouch = true;
-        doubleTouch = false;
-    }
-    if (event.touches.length >= 2) {
-        singleTouch = false;
-        doubleTouch = true;
-    }
-
-    // store the last touches
-    prevTouches[0] = event.touches[0];
-    prevTouches[1] = event.touches[1];
-
-}
-function onTouchMove(event) {
-    // get first touch coordinates
-    const touch0X = event.touches[0].offsetX;
-    const touch0Y = event.touches[0].offsetY;
-    const prevTouch0X = prevTouches[0].offsetX;
-    const prevTouch0Y = prevTouches[0].offsetY;
-
-    const scaledX = toTrueX(touch0X);
-    const scaledY = toTrueY(touch0Y);
-    const prevScaledX = toTrueX(prevTouch0X);
-    const prevScaledY = toTrueY(prevTouch0Y);
-
-    if (singleTouch) {
-        // add to history
-        drawings.push({
-            x0: prevScaledX,
-            y0: prevScaledY,
-            x1: scaledX,
-            y1: scaledY
-        })
-        drawLine(prevTouch0X, prevTouch0Y, touch0X, touch0Y);
-    }
-
-    if (doubleTouch) {
-        // get second touch coordinates
-        const touch1X = event.touches[1].offsetX;
-        const touch1Y = event.touches[1].offsetY;
-        const prevTouch1X = prevTouches[1].offsetX;
-        const prevTouch1Y = prevTouches[1].offsetY;
-
-        // get midpoints
-        const midX = (touch0X + touch1X) / 2;
-        const midY = (touch0Y + touch1Y) / 2;
-        const prevMidX = (prevTouch0X + prevTouch1X) / 2;
-        const prevMidY = (prevTouch0Y + prevTouch1Y) / 2;
-
-        // calculate the distances between the touches
-        const hypot = Math.sqrt(Math.pow((touch0X - touch1X), 2) + Math.pow((touch0Y - touch1Y), 2));
-        const prevHypot = Math.sqrt(Math.pow((prevTouch0X - prevTouch1X), 2) + Math.pow((prevTouch0Y - prevTouch1Y), 2));
-
-        // calculate the screen scale change
-        var zoomAmount = hypot / prevHypot;
-        scale = scale * zoomAmount;
-        const scaleAmount = 1 - zoomAmount;
-
-        // calculate how many pixels the midpoints have moved in the x and y direction
-        const panX = midX - prevMidX;
-        const panY = midY - prevMidY;
-        // scale this movement based on the zoom level
-        offsetX += (panX / scale);
-        offsetY += (panY / scale);
-
-        // Get the relative position of the middle of the zoom.
-        // 0, 0 would be top left. 
-        // 0, 1 would be top right etc.
-        var zoomRatioX = midX / canvas.clientWidth;
-        var zoomRatioY = midY / canvas.clientHeight;
-
-        // calculate the amounts zoomed from each edge of the screen
-        const unitsZoomedX = trueWidth() * scaleAmount;
-        const unitsZoomedY = trueHeight() * scaleAmount;
-
-        const unitsAddLeft = unitsZoomedX * zoomRatioX;
-        const unitsAddTop = unitsZoomedY * zoomRatioY;
-
-        offsetX += unitsAddLeft;
-        offsetY += unitsAddTop;
-
-        redrawCanvas();
-    }
-    prevTouches[0] = event.touches[0];
-    prevTouches[1] = event.touches[1];
-}
-function onTouchEnd(event) {
-    singleTouch = false;
-    doubleTouch = false;
-}
