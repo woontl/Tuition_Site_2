216784119b721a483de44e38e7a5c1be89c0ecbb
Woon Tian Li <woontl96@gmail.com>
2023-01-06 16:06:10 +0800

    updated whiteboard. Ipad support

--- flaskblog/static/javascripts/whiteboard.js
+++ flaskblog/static/javascripts/whiteboard.js
@@ -1,4 +1,4 @@
-
+// resizing
 const canvas_parent = document.querySelector("#canvas_parent")
 function onResize() {
     canvas.height = window.innerHeight;
@@ -20,8 +20,7 @@ onResize();
   
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
-    ctx.strokeStyle = "#222222";
-    ctx.lineWidth = 2;
+
   
     var drawing = false;
     var mousePos = {
@@ -90,9 +89,12 @@ onResize();
   
     function renderCanvas() {
       if (drawing) {
+        ctx.strokeStyle = currentcolor
+        ctx.lineWidth = currentfont
         ctx.moveTo(lastPos.x, lastPos.y);
         ctx.lineTo(mousePos.x, mousePos.y);
         ctx.stroke();
+        ctx.beginPath()
         lastPos = mousePos;
       }
     }
@@ -117,26 +119,115 @@ onResize();
     (function drawLoop() {
       requestAnimFrame(drawLoop);
       renderCanvas();
-    })();
-  
-    function clearCanvas() {
-      canvas.width = canvas.width;
+    })();  
+
+    function clearBoard() {
+        canvas.width = canvas.width;
+    };
+
+    function saveURL() {
+        var canvas_out = document.querySelector("#canvas");
+        var dataURL = canvas_out.toDataURL('image/png');
+        document.getElementById('workings').value = dataURL;
     }
-  
+
+    function eraseBoard() {
+        if (eraser_state == false) {
+            temp_color = currentcolor
+            temp_font = currentfont
+            currentcolor = 'white'
+            currentfont = 5
+            eraser_state = true;
+            eraserBtn.style.backgroundColor = "blue"
+            eraserBtn.style.border = "1px solid blue"
+        } else if (eraser_state == true) {
+            currentcolor = temp_color;
+            currentfont = temp_font;
+            currentfont = 1
+            eraser_state = false;
+            eraserBtn.style.backgroundColor = "white"
+            eraserBtn.style.border = "1px solid white"
+        } 
+    };
+
+    function changeblackColor() {
+        currentcolor = 'black';
+        colorBtn.style.backgroundColor = currentcolor
+        colorBtn.style.border = "1px solid" + currentcolor
+    };
+    function changeredColor() {
+        currentcolor = 'red';
+        colorBtn.style.backgroundColor = currentcolor
+        colorBtn.style.border = "1px solid" + currentcolor
+    };
+    function changeblueColor() {
+        currentcolor = 'blue';
+        colorBtn.style.backgroundColor = currentcolor
+        colorBtn.style.border = "1px solid" + currentcolor  
+        
+    };
+    function changegreenColor() {
+        currentcolor = 'green';
+        colorBtn.style.backgroundColor = currentcolor
+        colorBtn.style.border = "1px solid" + currentcolor  
+        
+    };
+    function changeyellowColor() {
+        currentcolor = 'yellow';
+        colorBtn.style.backgroundColor = currentcolor
+        colorBtn.style.border = "1px solid" + currentcolor  
+        
+    };
+
+    function changesmallfont() {
+        currentfont = 1
+    };
+    function changemediumfont() {
+        currentfont = 5
+    };
+    function changelargefont() {
+        currentfont = 10
+    };
+
+    //reload canvas with dataURL
+    var url = document.getElementById('workings').value;
+    var img = new Image()
+    img.src = url
+    img.onload = () => { ctx.drawImage(img, 0, 0); };
+    img.src = url
+
     // Set up the UI
-    var sigText = document.getElementById("sig-dataUrl");
-    var sigImage = document.getElementById("sig-image");
-    var clearBtn = document.getElementById("sig-clearBtn");
-    var submitBtn = document.getElementById("sig-submitBtn");
-    clearBtn.addEventListener("click", function(e) {
-      clearCanvas();
-      sigText.innerHTML = "Data URL for your signature will go here!";
-      sigImage.setAttribute("src", "");
-    }, false);
-    submitBtn.addEventListener("click", function(e) {
-      var dataUrl = canvas.toDataURL();
-      sigText.innerHTML = dataUrl;
-      sigImage.setAttribute("src", dataUrl);
-    }, false);
-  
+    var eraser_state = false;
+    var clearBtn = document.getElementById("canvas-clear-btn");
+    var submitBtn = document.getElementById("ans");
+    let temp_color = ''
+    let temp_font = ''
+    var saveBtn = document.getElementById("canvas-save-btn");
+    var refreshBtn = document.getElementById("canvas-refresh-btn");
+    var eraserBtn = document.getElementById("canvas-eraser-btn");
+    var colorBtn = document.getElementById("dropdownMenuButton-color");
+    var blackcolorBtn = document.getElementById("canvas-black-color-btn");
+    var redcolorBtn = document.getElementById("canvas-red-color-btn");
+    var bluecolorBtn = document.getElementById("canvas-blue-color-btn");
+    var greencolorBtn = document.getElementById("canvas-green-color-btn");
+    var yellowcolorBtn = document.getElementById("canvas-yellow-color-btn");
+    var currentcolor = 'black';
+    var currentfont = 1;
+    var fontBtn = document.getElementById("dropdownMenuButton-font");
+    var smallfontBtn = document.getElementById("canvas-small-font-btn");
+    var mediumfontBtn = document.getElementById("canvas-medium-font-btn");
+    var largefontBtn = document.getElementById("canvas-large-font-btn");
+
+    clearBtn.addEventListener("click", clearBoard)
+    submitBtn.addEventListener("click", saveURL)
+    eraserBtn.addEventListener("click", eraseBoard)
+    blackcolorBtn.addEventListener("click", changeblackColor)
+    redcolorBtn.addEventListener("click", changeredColor)
+    bluecolorBtn.addEventListener("click", changeblueColor)
+    greencolorBtn.addEventListener("click", changegreenColor)
+    yellowcolorBtn.addEventListener("click", changeyellowColor)
+    smallfontBtn.addEventListener("click", changesmallfont)
+    mediumfontBtn.addEventListener("click", changemediumfont)
+    largefontBtn.addEventListener("click", changelargefont)
+
   })();
