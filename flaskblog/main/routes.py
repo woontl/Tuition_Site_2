0263a1c92e9a363be764e1674f396f9892f19e25
woontl <woontl96@gmail.com>
2023-07-08 14:34:29 +0800

    Revert "v2.0.0 test2"
    This reverts commit d4af597a0186867daefa543ed94cad1212852473.

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -75,8 +75,6 @@ def home():
         id_arr = [homework.id]
     if note == None:
         note = []
-    if lesson == None:
-        lesson = []
     date_now = datetime.datetime.now()
     try:
         homework_due_days = round((homework.deadline - date_now) / timedelta(days=1))
